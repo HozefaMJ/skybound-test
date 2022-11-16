d62c9795d714ab0dfe3040f38909cc0535cc4b0b
HozefaMJ <hozefa24imp@gmail.com>
2022-11-16 21:14:04 +0530

    #3 Static HomeScreen & QuoteBox Component

--- src/components/QuoteBox/QuoteBox.tsx
+++ src/components/QuoteBox/QuoteBox.tsx
@@ -1,12 +1,35 @@
 import * as React from 'react';
 
-export interface IAppProps {
+import './styles.css';
+
+export interface QuoteProps {
+    quoteCategory: string;
+    quoteText: string;
+    author: string;
+    tags: (string)[];
+    background: string
 }
 
-const QuoteBox = (props: IAppProps) => {
+const QuoteBox = ({quoteCategory,quoteText,author,tags,background}: QuoteProps) => {
   return (
-    <div>
-      <h2>Quote Box</h2>
+    <div className='quoteBox'>
+        <div style={{
+                backgroundImage: `url("${background}")`
+            }} className="quoteContainer">
+            <h4 className='quoteCategory'>
+                <span style={{padding:'7px',borderRadius:'10px',backgroundColor:'#000'}}>{quoteCategory}</span>
+            </h4>
+            <h2 className='quoteText'>{quoteText}</h2>
+        </div>
+        <div className='tagAuthorBox'>
+            <span className='tags'>
+                {tags.map((tag,index) => {
+                    return (
+                        index === tags.length - 1 ? (<p key={tag}>{tag}.</p>) : <p key={tag}>{tag},</p>)
+                })}
+            </span>
+            <h3 className='authorText'>- {author}</h3>
+        </div>
     </div>
   );
 }
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,22 +1,20 @@
-import * as React from 'react';
-import QuoteBox from '../../components/QuoteBox/QuoteBox';
+import React from "react";
 
-export interface IAppProps {
+import QuoteBox from "../../components/QuoteBox/QuoteBox";
+
+const HomeScreen = () => {
+    return (
+        <div>
+            <h1>SKYBOUND</h1>
+            <QuoteBox 
+                quoteCategory={'Inspire'} 
+                quoteText="Meaning is something you build into your life. You build it out of your own past, out of your affections and loyalties, out of the experience of humankind as it is passed on to you... You are the only one who can put them together into that unique pattern that will be your life."
+                background="https://images.unsplash.com/photo-1494438639946-1ebd1d20bf85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=867&q=80"
+                tags={['tags','tgga','new','trending']}
+                author="John Gardner"
+            />
+        </div>
+    )
 }
-
-const HomeScreen = (props: IAppProps) => {
-
-  React.useEffect(() => {
-    console.log('Err Solved')
-  })
-
-  return (
-    <div>
-      <h1>Skybound Assesment</h1>
-      <QuoteBox/>
-    </div>
-  );
-}
-
 
 export default HomeScreen;
